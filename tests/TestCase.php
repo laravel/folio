e7b13496abd28ea7d2dfd2ab0dee4658b6e8edc6
Nuno Maduro <enunomaduro@gmail.com>
2023-03-03 21:14:22 +0000

    Uses Pest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
+use Illuminate\Filesystem\Filesystem;
 use Laravel\Folio\FolioServiceProvider;
+use Laravel\Folio\Router;
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 
 abstract class TestCase extends OrchestraTestCase
@@ -11,12 +13,39 @@ abstract class TestCase extends OrchestraTestCase
      * Get the package providers.
      *
      * @param  \Illuminate\Foundation\Application  $app
-     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
      */
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             FolioServiceProvider::class,
         ];
+    }
+
+    /**
+     * Create the given views.
+     *
+     * @param  array<string, array<string, string>|string>  $views
+     */
+    protected function views(array $views, $directory = null): void
+    {
+        $directory ??= __DIR__.'/fixtures/views';
+
+        foreach ($views as $key => $value) {
+            if (is_array($value)) {
+                (new Filesystem)->makeDirectory($directory.$key);
+
+                $this->views($value, $directory.$key);
+            } else {
+                touch($directory.$value);
+            }
+        }
+    }
+
+    /**
+     * Create a new router instance.
+     */
+    protected function router(): Router
+    {
+        return new Router([__DIR__.'/fixtures/views']);
     }
 }

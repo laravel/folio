90a28c9ab030b050a888e477389c01cd4890d144
Taylor Otwell <taylor@laravel.com>
2023-03-14 16:41:02 -0500

    wip

--- tests/Feature/FolioIntegrationTest.php
+++ tests/Feature/FolioIntegrationTest.php
@@ -3,11 +3,23 @@
 use Laravel\Folio\Folio;
 
 beforeEach(function () {
-    Folio::route(__DIR__.'/resources/views/pages', middleware: []);
+    $_SERVER['__folio_users_middleware'] = false;
+
+    Folio::route(__DIR__.'/resources/views/pages', middleware: [
+        '/users/*' => [
+            function ($request, $next) {
+                $_SERVER['__folio_users_middleware'] = true;
+
+                return $next($request);
+            }
+        ],
+    ]);
 });
 
 test('root index view can be matched', function () {
     $response = $this->get('/users/Taylor');
 
     $response->assertSee('Hello, Taylor');
+
+    $this->assertTrue($_SERVER['__folio_users_middleware']);
 })->only();

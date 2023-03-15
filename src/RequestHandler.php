ac35e24de0594aabd60ea66d5f391f57efeab8c5
Taylor Otwell <taylor@laravel.com>
2023-03-15 16:16:06 -0500

    wip

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -6,33 +6,33 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Routing\Pipeline;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
+use Laravel\Folio\Pipeline\MatchedView;
 
 class RequestHandler
 {
+    /**
+     * Create a new request handler instance.
+     */
     public function __construct(protected MountPath $mountPath,
                                 protected ?Closure $renderUsing = null)
     {
     }
 
-    public function __invoke(Request $request, string $uri)
+    /**
+     * Handle the incoming request using Folio.
+     */
+    public function __invoke(Request $request, string $uri): Response
     {
         $matchedView = (new Router(
             $this->mountPath->path
         ))->resolve($uri) ?? abort(404);
 
-        $middleware = $this->mountPath
-                ->middleware
-                ->match($matchedView)
-                ->prepend('web')
-                ->merge($matchedView->inlineMiddleware())
-                ->unique()
-                ->values();
-
         return (new Pipeline(app()))
             ->send($request)
-            ->through(Route::resolveMiddleware($middleware->all()))
+            ->through($this->middleware($matchedView))
             ->then(function ($request) use ($matchedView) {
                 if ($this->renderUsing) {
                     return call_user_func($this->renderUsing, $request, $matchedView);
@@ -47,5 +47,17 @@ class RequestHandler
                     ]
                 );
             });
+    }
+
+    protected function middleware(MatchedView $matchedView): array
+    {
+        return Route::resolveMiddleware($this->mountPath
+                ->middleware
+                ->match($matchedView)
+                ->prepend('web')
+                ->merge($matchedView->inlineMiddleware())
+                ->unique()
+                ->values()
+                ->all());
     }
 }

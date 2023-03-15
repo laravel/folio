af39a3d5070061240d3ad3838f16e55e396696d3
Taylor Otwell <taylor@laravel.com>
2023-03-15 15:38:01 -0500

    wip

--- tests/Feature/FolioIntegrationTest.php
+++ tests/Feature/FolioIntegrationTest.php
@@ -4,12 +4,20 @@ use Laravel\Folio\Folio;
 
 beforeEach(function () {
     $_SERVER['__folio_users_middleware'] = false;
+    $_SERVER['__folio_flights_middleware'] = false;
     $_SERVER['__folio_flights_inline_middleware'] = false;
 
     Folio::route(__DIR__.'/resources/views/pages', middleware: [
         '/users/*' => [
             function ($request, $next) {
                 $_SERVER['__folio_users_middleware'] = true;
+
+                return $next($request);
+            }
+        ],
+        '/flights/*' => [
+            function ($request, $next) {
+                $_SERVER['__folio_flights_middleware'] = true;
 
                 return $next($request);
             }
@@ -30,6 +38,7 @@ test('inline middleware are invoked', function () {
 
     $response->assertSee('Flight Index');
 
+    $this->assertTrue($_SERVER['__folio_flights_middleware']);
     $this->assertTrue($_SERVER['__folio_flights_inline_middleware']);
 });
 
@@ -38,6 +47,15 @@ test('middleware can be retrieved for a given uri', function () {
 
     $this->assertCount(1, $middleware);
     $this->assertTrue($middleware[0] instanceof Closure);
-    $this->assertEmpty(Folio::middlewareFor('/flights/1'));
+    $this->assertEmpty(Folio::middlewareFor('/missing/1'));
 });
 
+
+test('inline can be retrieved for a given uri', function () {
+    $middleware = Folio::middlewareFor('/flights');
+
+    $this->assertCount(2, $middleware);
+    $this->assertTrue($middleware[0] instanceof Closure);
+    $this->assertTrue($middleware[1] instanceof Closure);
+    $this->assertEmpty(Folio::middlewareFor('/missing/1'));
+});

c0172ba99ffe9f3945851136ff38cf587cbe363f
Taylor Otwell <taylor@laravel.com>
2023-03-16 00:16:23 -0500

    wip

--- tests/Feature/FolioIntegrationTest.php
+++ tests/Feature/FolioIntegrationTest.php
@@ -1,28 +1,11 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 use Laravel\Folio\Folio;
-use Tests\Feature\Fixtures\Podcast;
 
 beforeEach(function () {
     $_SERVER['__folio_users_middleware'] = false;
     $_SERVER['__folio_flights_middleware'] = false;
     $_SERVER['__folio_flights_inline_middleware'] = false;
-
-    Schema::create('podcasts', function ($table) {
-        $table->id();
-        $table->string('name');
-        $table->timestamps();
-        $table->softDeletes();
-    });
-
-    Schema::create('comments', function ($table) {
-        $table->id();
-        $table->foreignId('podcast_id');
-        $table->string('content');
-        $table->timestamps();
-    });
 
     Folio::route(__DIR__.'/resources/views/pages', middleware: [
         '/users/*' => [
@@ -74,68 +57,4 @@ test('inline middleware can be retrieved for a given uri', function () {
     $this->assertTrue($middleware[1] instanceof Closure);
 
     $this->assertEmpty(Folio::middlewareFor('/missing/1'));
-});
-
-test('implicit bindings are resolved', function () {
-    $podcast = Podcast::create([
-        'name' => 'test-podcast-name',
-    ]);
-
-    $this->get('/podcasts/'.$podcast->id)->assertSee('test-podcast-name');
-});
-
-test('child implicit bindings are scoped to the parent if field is present', function () {
-    $podcast = Podcast::create([
-        'name' => 'test-podcast-name',
-    ])->fresh();
-
-    $comment = $podcast->comments()->create([
-        'content' => 'test-comment-content',
-    ])->fresh();
-
-    $secondPodcast = Podcast::create([
-        'name' => 'another-podcast-name',
-    ])->fresh();
-
-    $secondComment = $secondPodcast->comments()->create([
-        'content' => 'another-comment-content',
-    ])->fresh();
-
-    $this->get('/podcasts/'.$podcast->id.'/comments/'.$secondComment->id)
-            ->assertNotFound();
-
-    $this->get('/podcasts/'.$podcast->id.'/comments/'.$comment->id)
-            ->assertSee('test-comment-content');
-});
-
-test('soft deletable bindings are not resolved if not allowed', function () {
-    $podcast = Podcast::create([
-        'name' => 'test-podcast-name',
-    ]);
-
-    $podcast->delete();
-
-    $this->get('/podcasts/'.$podcast->id)->assertNotFound();
-});
-
-test('soft deletable bindings are resolved if allowed', function () {
-    $podcast = Podcast::create([
-        'name' => 'test-podcast-name',
-    ]);
-
-    $podcast->delete();
-
-    $this->get('/deleted-podcasts/'.$podcast->id)->assertSee('test-podcast-name');
-});
-
-test('enums can be injected', function () {
-    $response = $this->get('/categories/posts');
-
-    $response->assertSee('posts');
-});
-
-test('not found error is generated if enum value is not valid', function () {
-    $response = $this->get('/categories/missing-category');
-
-    $response->assertNotFound();
 });
---
+++ tests/Feature/FolioModelBindingIntegrationTest.php
@@ -0,0 +1,88 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Laravel\Folio\Folio;
+use Tests\Feature\Fixtures\Podcast;
+
+beforeEach(function () {
+    Schema::create('podcasts', function ($table) {
+        $table->id();
+        $table->string('name');
+        $table->timestamps();
+        $table->softDeletes();
+    });
+
+    Schema::create('comments', function ($table) {
+        $table->id();
+        $table->foreignId('podcast_id');
+        $table->string('content');
+        $table->timestamps();
+    });
+
+    Folio::route(__DIR__.'/resources/views/pages');
+});
+
+test('implicit bindings are resolved', function () {
+    $podcast = Podcast::create([
+        'name' => 'test-podcast-name',
+    ]);
+
+    $this->get('/podcasts/'.$podcast->id)->assertSee('test-podcast-name');
+});
+
+test('child implicit bindings are scoped to the parent if field is present', function () {
+    $podcast = Podcast::create([
+        'name' => 'test-podcast-name',
+    ])->fresh();
+
+    $comment = $podcast->comments()->create([
+        'content' => 'test-comment-content',
+    ])->fresh();
+
+    $secondPodcast = Podcast::create([
+        'name' => 'another-podcast-name',
+    ])->fresh();
+
+    $secondComment = $secondPodcast->comments()->create([
+        'content' => 'another-comment-content',
+    ])->fresh();
+
+    $this->get('/podcasts/'.$podcast->id.'/comments/'.$secondComment->id)
+            ->assertNotFound();
+
+    $this->get('/podcasts/'.$podcast->id.'/comments/'.$comment->id)
+            ->assertSee('test-comment-content');
+});
+
+test('soft deletable bindings are not resolved if not allowed', function () {
+    $podcast = Podcast::create([
+        'name' => 'test-podcast-name',
+    ]);
+
+    $podcast->delete();
+
+    $this->get('/podcasts/'.$podcast->id)->assertNotFound();
+});
+
+test('soft deletable bindings are resolved if allowed', function () {
+    $podcast = Podcast::create([
+        'name' => 'test-podcast-name',
+    ]);
+
+    $podcast->delete();
+
+    $this->get('/deleted-podcasts/'.$podcast->id)->assertSee('test-podcast-name');
+});
+
+test('enums can be injected', function () {
+    $response = $this->get('/categories/posts');
+
+    $response->assertSee('posts');
+});
+
+test('not found error is generated if enum value is not valid', function () {
+    $response = $this->get('/categories/missing-category');
+
+    $response->assertNotFound();
+});

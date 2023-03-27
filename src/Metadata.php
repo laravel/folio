5009c9f091f98445bb2aed82548ee7679969f266
Nuno Maduro <enunomaduro@gmail.com>
2023-03-27 11:05:55 +0100

    Removes dead code

--- src/Metadata.php
+++ src/Metadata.php
@@ -7,16 +7,12 @@ use Illuminate\Support\Collection;
 class Metadata
 {
     /**
-     * The middleware specified in the metadata.
-     */
-    public Collection $middleware;
-
-    /**
      * Create a new metadata instance.
      */
-    public function __construct(?Collection $middleware = null,
-                                public bool $withTrashed = false)
-    {
-        $this->middleware = $middleware ?? new Collection;
+    public function __construct(
+        public Collection $middleware = new Collection,
+        public bool $withTrashed = false
+    ) {
+        //
     }
 }

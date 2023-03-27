28f4e1637ad551635f230cd07698bcd5bd95f6a2
Nuno Maduro <enunomaduro@gmail.com>
2023-03-27 10:34:12 +0100

    Ensures functions do not exist

--- functions.php
+++ functions.php
@@ -7,14 +7,16 @@ use Illuminate\Container\Container;
 use Illuminate\Support\Arr;
 use Laravel\Folio\Exceptions\MetadataIntercepted;
 
-function folio(Closure|string|array $middleware = [], bool $withTrashed = false)
-{
-    if (Container::getInstance()->make(InlineMetadataInterceptor::class)->listening()) {
-        throw new MetadataIntercepted(
-            new Metadata(
-                middleware: collect(Arr::wrap($middleware)),
-                withTrashed: $withTrashed,
-            )
-        );
+if (! function_exists('folio')) {
+    function folio(Closure|string|array $middleware = [], bool $withTrashed = false)
+    {
+        if (Container::getInstance()->make(InlineMetadataInterceptor::class)->listening()) {
+            throw new MetadataIntercepted(
+                new Metadata(
+                    middleware: collect(Arr::wrap($middleware)),
+                    withTrashed: $withTrashed,
+                )
+            );
+        }
     }
 }

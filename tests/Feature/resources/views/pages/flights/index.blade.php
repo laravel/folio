120c50b8cbaa0af0a515ae836edfacc705fce43f
Nuno Maduro <enunomaduro@gmail.com>
2023-05-04 16:57:23 +0100

    Pints code

--- tests/Feature/resources/views/pages/flights/index.blade.php
+++ tests/Feature/resources/views/pages/flights/index.blade.php
@@ -3,6 +3,7 @@ use function Laravel\Folio\{page};
 
 page(middleware: [function ($request, $next) {
     $_SERVER['__folio_flights_inline_middleware'] = true;
+
     return $next($request);
 }]);
 ?>

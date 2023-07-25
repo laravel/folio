5e3da012354d4a9632c7538fff9fd78581f235df
Nuno Maduro <enunomaduro@gmail.com>
2023-07-25 13:33:55 +0100

    Fix static analysis

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -57,7 +57,7 @@ class InstallCommand extends Command
     protected function registerFolioServiceProvider(): void
     {
         if (method_exists(ServiceProvider::class, 'addProviderToBootstrapFile') &&
-            ServiceProvider::addProviderToBootstrapFile(\App\Providers\FolioServiceProvider::class)) {
+            ServiceProvider::addProviderToBootstrapFile(\App\Providers\FolioServiceProvider::class)) { // @phpstan-ignore-line
             return;
         }
 

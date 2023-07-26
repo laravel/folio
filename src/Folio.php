12742db8bd1f792e0ed2d7e8b0beac7a8fa50aee
Ngo Quoc Dat <datlechin@gmail.com>
2023-07-26 21:23:56 +0700

    Update facade docblock

--- src/Folio.php
+++ src/Folio.php
@@ -5,13 +5,17 @@ namespace Laravel\Folio;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Laravel\Folio\FolioManager route(?string $path = null, ?string $uri = '/', array $middleware = [])
+ * @method static static route(string|null $path = null, string|null $uri = '/', array $middleware = [])
+ * @method static array middlewareFor(string $uri)
+ * @method static mixed|null data(string|null $key = null, mixed|null $default = null)
+ * @method static \Laravel\Folio\FolioManager renderUsing(\Closure|null $callback = null)
+ * @method static array mountPaths()
+ * @method static array paths()
+ *
+ * @see \Laravel\Folio\FolioManager
  */
 class Folio extends Facade
 {
-    /**
-     * {@inheritDoc}
-     */
     public static function getFacadeAccessor(): string
     {
         return FolioManager::class;

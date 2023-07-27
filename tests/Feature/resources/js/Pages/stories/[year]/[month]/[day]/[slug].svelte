eb8d5bb351dfd9386d9bbe9b6fa0767bf790be99
Zeb <burroughszeb@me.com>
2023-07-26 22:10:42 -0500

    add test folders

---
+++ tests/Feature/resources/js/Pages/stories/[year]/[month]/[day]/[slug].svelte
@@ -0,0 +1,17 @@
+<script>
+    import { page } from '@inertiajs/svelte'
+</script>
+
+<dl>
+    <dt>Year</dt>
+    <dd>{$page.props.year}</dd>
+
+    <dt>Month</dt>
+    <dd>{$page.props.month}</dd>
+
+    <dt>Day</dt>
+    <dd>{$page.props.day}</dd>
+
+    <dt>Slug</dt>
+    <dd>{$page.props.slug}</dd>
+  </dl>

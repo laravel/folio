63969a03fe198ac5e14f451809cc3d8fc2c32081
Ryan Chandler <ryangjchandler@gmail.com>
2023-07-31 09:55:21 +0100

    chore: add description to UrlGenerationException

--- src/Exceptions/UrlGenerationException.php
+++ src/Exceptions/UrlGenerationException.php
@@ -6,6 +6,9 @@ use Exception;
 
 class UrlGenerationException extends Exception
 {
+    /**
+     * Create a new exception for a missing URL parameter.
+     */
     public static function forMissingParameter(string $path, string $parameter): static
     {
         return new static(sprintf(

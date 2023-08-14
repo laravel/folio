e940a1686fbb762dcf65fba8a51cbc2b88e59337
Taylor Otwell <taylor@laravel.com>
2023-08-14 17:03:06 -0500

    different approach to url decoration

--- src/FolioServiceProvider.php
+++ src/FolioServiceProvider.php
@@ -4,8 +4,9 @@ namespace Laravel\Folio;
 
 use Illuminate\Console\Events\CommandFinished;
 use Illuminate\Console\Events\CommandStarting;
-use Illuminate\Contracts\Events\Dispatcher;
-use Illuminate\Contracts\Routing\UrlGenerator;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class FolioServiceProvider extends ServiceProvider
@@ -33,6 +34,7 @@ class FolioServiceProvider extends ServiceProvider
         $this->registerPublishing();
         $this->registerUrlGenerator();
         $this->registerTerminationCallback();
+        $this->cacheFolioRoutesOnRouteCache();
     }
 
     /**
@@ -46,20 +48,6 @@ class FolioServiceProvider extends ServiceProvider
                 Console\ListCommand::class,
                 Console\MakeCommand::class,
             ]);
-
-            with($this->app->make('events'), function (Dispatcher $events) {
-                $events->listen(CommandStarting::class, function (CommandStarting $event) {
-                    if ($event->command === 'route:clear') {
-                        $this->app->make(FolioRoutes::class)->flush();
-                    }
-                });
-
-                $events->listen(CommandFinished::class, function (CommandFinished $event) {
-                    if ($event->command === 'route:cache') {
-                        $this->app->make(FolioRoutes::class)->persist();
-                    }
-                });
-            });
         }
     }
 
@@ -76,12 +64,18 @@ class FolioServiceProvider extends ServiceProvider
     }
 
     /**
-     * Register the URL generator decorator.
+     * Register the URL generator route resolver.
      */
     protected function registerUrlGenerator(): void
     {
-        $this->app->extend('url', function (UrlGenerator $urlGenerator) {
-            return new UrlGeneratorDecorator($urlGenerator, $this->app->make(FolioRoutes::class));
+        $this->app->afterResolving(UrlGenerator::class, function ($url) {
+            $url->resolveMissingNamedRoutesUsing(function ($name, $parameters, $absolute) {
+                $routes = app(FolioRoutes::class);
+
+                if ($routes->has($name)) {
+                    return $routes->get($name, $parameters, $absolute);
+                }
+            });
         });
     }
 
@@ -91,5 +85,23 @@ class FolioServiceProvider extends ServiceProvider
     protected function registerTerminationCallback(): void
     {
         $this->app->terminating(fn (FolioManager $manager) => $manager->terminate());
+    }
+
+    /**
+     * Cache Folio's routes when the route:cache and route:clear commands are run.
+     */
+    protected function cacheFolioRoutesOnRouteCache(): void
+    {
+        Event::listen(CommandFinished::class, function (CommandFinished $event) {
+            if ($event->command === 'route:cache') {
+                $this->app->make(FolioRoutes::class)->persist();
+            }
+        });
+
+        Event::listen(CommandStarting::class, function (CommandStarting $event) {
+            if ($event->command === 'route:clear') {
+                $this->app->make(FolioRoutes::class)->flush();
+            }
+        });
     }
 }

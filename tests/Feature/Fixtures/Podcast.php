3070c3b7755cce732514cb573a5f79927e5585bc
taylorotwell <taylorotwell@users.noreply.github.com>
2023-09-25 00:48:28 +0000

    Fix code styling

--- tests/Feature/Fixtures/Podcast.php
+++ tests/Feature/Fixtures/Podcast.php
@@ -3,8 +3,8 @@
 namespace Tests\Feature\Fixtures;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Podcast extends Model
 {
